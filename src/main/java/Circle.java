a2cc17177f41b68fc80d8e4d8a7d33caea4fbbad
Jenny <euro1000@stud.ek.dk>
2026-04-24 13:41:02 +0200

    circle class

---
+++ src/main/java/Circle.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Circle implements Shape {
+    public double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircleCircumference(double radius) {
+        return 2 * Math.PI * radius;
+    }
+
+}
