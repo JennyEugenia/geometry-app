711c499855f8e2e24e16c822e5a7c424fe9c16be
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-24 14:17:20 +0200

    Circle and Rectangle with implements Shape

---
+++ src/main/java/Circle.java
@@ -0,0 +1,21 @@
+public class Circle implements Shape{
+
+/*
+    public double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircleCircumference(double radius) {
+        return 2 * Math.PI * radius;
+    }*/
+
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    public double calculateCircumference() {
+        return 0;
+    }
+}
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle implements Shape {
+
+/*public double calculateRectangleArea(double width, double height) {
+        return width * height;
+    }
+
+    public double calculateRectanglePerimeter(double width, double height) {
+        return 2 * (width + height);
+    }*/
+
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    public double calculateCircumference() {
+        return 0;
+    }
+}
