da96f0a0afd80c60a894a71745995f150e9fec91
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-26 22:27:19 +0200

    Make logik to menu

--- src/main/java/ConsoleUI.java
+++ src/main/java/ConsoleUI.java
@@ -4,6 +4,7 @@ public class ConsoleUI {
 
     private Scanner scanner = new Scanner(System.in);
 
+
     public int start() {
         System.out.println("\n 🧮 Geometry Calculator 🧮 ");
         System.out.println("1. Circle");
@@ -11,6 +12,51 @@ public class ConsoleUI {
         System.out.println("3. Triangle ");
         System.out.println("0. Exit");
 
-        return scanner.nextInt();
+        int choice = readInt();
+
+        try {
+            switch (choice) {
+                case 1 -> circle();
+                case 2 -> rectangle();
+                case 3 -> triangle();
+                case 4 -> {
+                    return choice;
+                }
+                default -> printError("Ugyldigt valg");
+            }
+        } catch (Exception e) {
+            printError(e.getMessage());
+        }
+        return choice;
     }
+
+    private void triangle() {
+    }
+
+    private void rectangle() {
+    }
+
+    private void circle() {
+        System.out.print("Inset radius number");
+        int number = scanner.nextInt();
+
+        Circle circle = new Circle(number);
+        circle.calculateArea();
+    }
+
+
+    private int readInt () {
+            while (!scanner.hasNextInt()) {
+                printError("Indtast et tal!");
+                scanner.next();
+            }
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            return number;
+        }
+
+        private void printError (String message){
+            System.out.println(" Fejl: " + message);
+        }
+
 }
