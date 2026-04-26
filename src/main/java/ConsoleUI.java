e210c72983daf31061867c7a028c13340cebaf16
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-26 21:53:11 +0200

    UI

--- src/main/java/ConsoleUI.java
+++ src/main/java/ConsoleUI.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public int menu() {
+        System.out.println("\n 🧮 Geometry Calculator 🧮 ");
+        System.out.println("1. Circle");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Triangle ");
+        System.out.println("0. Exit");
+
+        return scanner.nextInt();
+    }
 }
