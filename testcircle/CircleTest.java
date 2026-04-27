9397e4f56f3ab921ca688ab3c58b5a058d8d0f25
Jenny <euro1000@stud.ek.dk>
2026-04-27 15:29:36 +0200

    new things in: main,shapefactory,triangel,pom

---
+++ testcircle/CircleTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+    //add delta- tolerance værdi
+    @Test
+    public void testCalculateArea() {
+        Circle c = new Circle( 1);
+       assertEquals(3.14, c.calculateArea(), 0.01);
+    }
+    @Test
+    //C= 2 *Pi*2 = 4Pi, dvs C = 4 * 3.1416 = 12.5664, estimate 13
+    public void testCalculateCircumference() {
+        Circle c = new Circle(2);
+        assertEquals(12.5, c.calculateCircumference(), 0.1);
+
+    }
+
+
+    }
+
+}
