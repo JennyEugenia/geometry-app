a94a3a41615b41cad21a0d5c19b24710f2b57f2c
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-27 10:07:03 +0200

    MAIN--

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        //WTF
                 ConsoleUI ui = new ConsoleUI();
                 ShapeFactory factory = new ShapeFactory();
                 GeometryController controller = new GeometryController(ui, factory);
