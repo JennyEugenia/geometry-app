bcd12e2950c41ea36f0eb3abb3c875dea32daffa
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-27 10:18:01 +0200

    have same methode problem from merge

--- src/main/java/ConsoleUI.java
+++ src/main/java/ConsoleUI.java
@@ -11,12 +11,6 @@ public class ConsoleUI {
         System.out.println("2. Rectangle");
         System.out.println("3. Triangle ");
         System.out.println("0. Exit");
-        public int getMenuChoice() {
-            System.out.println("\n🧮 Geometry Calculator 🧮");
-            System.out.println("1. Circle");
-            System.out.println("2. Rectangle");
-            System.out.println("3. Triangle");
-            System.out.println("0. Exit");
 
             return readInt();
         }
