50c02952a5796d41ada58c1bc245a474b1670ecf
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-27 10:01:49 +0200

    console-ui-new-method

---
+++ src/main/java/GeometryController.java
@@ -0,0 +1,30 @@
+public class GeometryController {
+
+    public class GeometryController {
+
+        private final ConsoleUI ui;
+        private final ShapeFactory factory;
+
+        public GeometryController(ConsoleUI ui, ShapeFactory factory) {
+            this.ui = ui;
+            this.factory = factory;
+        }
+
+        public void run() {
+            while (true) {
+
+                int choice = ui.getMenuChoice();
+
+                if (choice == 0) {
+                    break;
+                }
+
+                double[] params = ui.getParameters(choice);
+
+                Shape shape = factory.createShape(choice, params);
+
+                ui.showResult(shape);
+            }
+        }
+    }
+}
