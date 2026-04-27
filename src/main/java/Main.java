6dbcda1f95bfad6994cfbf8108a7521f94626b7f
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-27 10:24:37 +0200

    changed again1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-//kode der skal refactors
+
 public class Main {
 
     public static void main(String[] args) {
-        //WTF
+
                 ConsoleUI ui = new ConsoleUI();
                 ShapeFactory factory = new ShapeFactory();
                 GeometryController controller = new GeometryController(ui, factory);
