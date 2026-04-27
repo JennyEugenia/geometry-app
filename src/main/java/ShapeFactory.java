96858b48546ffa8fb29b7def7455e5acc1d7cce6
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-04-27 09:53:15 +0200

    ConsoleUI1 change

--- src/main/java/ShapeFactory.java
+++ src/main/java/ShapeFactory.java
@@ -1,2 +1,21 @@
 public class ShapeFactory {
+
+    public Shape createShape(int type, double[] params) {
+
+        switch (type) {
+
+            case "circle":
+                return new Circle(params[0]);
+
+            case "rectangle":
+                return new Rectangle(params[0], params[1]);
+
+            case "triangle":
+                return new Triangle(params[0], params[1], params[2]);
+
+            default:
+                throw new IllegalArgumentException("Unknown shape: " + type);
+        }
+    }
 }
+
